100196cc08cbd22929343fd2291087c84e4f1500
balint144@gmail.com <balint144@gmail.com>
2019-10-22 21:01:11 +0200

    szolgáltatások continue

---
+++ szolgaltatasok/fascia-smr-training/index.php
@@ -0,0 +1,69 @@
+<?php 
+$_GET['menu'] = 'services';
+$_GET['submenu'] = 'Fascia / SMR training';
+$_GET['title'] = 'Fascia / SMR training';
+include('../../config.php');
+include('../../head.php');
+?>
+			<!-- contain main informative part of the site -->
+			<main id="main" role="main" style="margin-top:86px;">
+                <!-- page banner -->
+				<header class="page-banner small grey">
+					<div class="container">
+						<div class="row">
+							<div class="col-xs-12">
+								<div class="holder">
+									<h1 class="heading text-uppercase">Fascia / SMR training</h1>
+								</div>
+								<?php include('../breadcrumb.php') ?>
+							</div>
+						</div>
+					</div>
+				</header>
+                <section class="core-section padding-top-100 padding-bottom-100">
+					<div class="container">
+						<div class="row">
+							<div class="col-xs-12 col-sm-8 col-md-9 col-sm-push-4 col-md-push-3">
+                            <div class="service-txt">
+									<h2>Fascia / SMR training</h2>
+									<ul class="list-unstyled list">
+                                        <li>Az
+                                            SMR hengeren való gurulással növelhető a izomszövetek vérellátása,
+                                            gyorsabban regenerálódnak az izmok, nő a flexibilitás és a
+                                            mozgástartomány, csökken a fájdalom.</li>
+                                        <li>SMR
+                                            (Self Myofascial Release) a saját izompólyánk fellazítását jelenti
+                                            különböző eszközök segítségével.</li>
+                                        <li>Fascia
+                                            / SMR tréning nem egyes izmokat, hanem a teljes fasciahálót, ezen belül is
+                                            a hosszú myofasciális láncokat edzi. Funkcionális mozgásokon alapul, izolált
+                                            mozdulatok ismételgetése helyett a mozgásminták fejlesztésére helyezi a
+                                            hangsúlyt. Az edzés módja is jelentősen eltér az eddig megszokottaktól,
+                                            jóval összetettebb azoknál.</li>
+                                        <li>Az
+                                            SMR henger a prevenció, az egészségmegőrzés és egyben a rehabilitáció kiváló
+                                            eszköze is, hiszen a önmasszázs technikán túl kiváló eszköz a mobilizáció,
+                                            a törzserő és az egyensúly fejlesztéséhez.&nbsp;</li>
+                                        <li>Használatával
+                                            jelentősen csökkenthetők izomfájdalmaink, javítható izmaink és ízületeink
+                                            rugalmassága. Minimális erőkifejtéssel, egyszerű, de mégis nagyon hatásos
+                                            gyakorlatokkal fokozhatjuk izmaink teljesítményét.</li>
+                                        <li>Különböző
+                                            keménységű és nagyságú SMR hengerek rendszeres és célzott alkalmazása
+                                            meggyorsítja az izmok regenerációs képességét, fellazítja a teljes testet,
+                                            segíti a kisugárzó fájdalmak elmúlását, az izomláz gyors és hatékony
+                                            kezelését.</li>
+                                        <li>Mozgásanyaga
+                                            könnyedén beilleszthető a különféle edzésprogramokba, a mozgásterápiába,
+                                            hisz nem helyettesíti, hanem kiegészíti azokat.</li>
+                                    </ul>
+                                </div>
+							</div>
+                            <?php include('../leftMenu.php') ?>
+						</div>
+					</div>
+                </section>
+			</main>			
+<?php
+include('../../footer.php');
+?>
--- szolgaltatasok/leftMenu.php
+++ szolgaltatasok/leftMenu.php
@@ -7,9 +7,9 @@
             <li class="<?php echo ($_GET['submenu'] == 'Baba-mama torna' ? 'active' : '') ?>"><a href="<?php echo $pageUrl; ?>szolgaltatasok/baba-mama-torna"><i class="fa fa-caret-right"></i> Baba-mama torna</a></li>
             <li class="<?php echo ($_GET['submenu'] == 'Baba-mama-papa torna' ? 'active' : '') ?>"><a href="<?php echo $pageUrl; ?>szolgaltatasok/baba-mama-papa-torna"><i class="fa fa-caret-right"></i> Baba-mama-papa torna</a></li>
             <li class="<?php echo ($_GET['submenu'] == 'Calisthenics/Streetworkout' ? 'active' : '') ?>"><a href="<?php echo $pageUrl; ?>szolgaltatasok/calisthenics-streetworkout"><i class="fa fa-caret-right"></i> Calisthenics/Streetworkout</a></li>
-            <li><a href="#"><i class="fa fa-caret-right"></i> Magazine (25)</a></li>
-            <li><a href="#"><i class="fa fa-caret-right"></i> Creative (23)</a></li>
-            <li><a href="#"><i class="fa fa-caret-right"></i> Winter(2)</a></li>
+            <li class="<?php echo ($_GET['submenu'] == 'Cross training' ? 'active' : '') ?>"><a href="<?php echo $pageUrl; ?>szolgaltatasok/cross-training"><i class="fa fa-caret-right"></i> Cross training</a></li>
+            <li class="<?php echo ($_GET['submenu'] == 'Fascia / SMR training' ? 'active' : '') ?>"><a href="<?php echo $pageUrl; ?>szolgaltatasok/fascia-smr-training"><i class="fa fa-caret-right"></i> Fascia / SMR training</a></li>
+            <li class="<?php echo ($_GET['submenu'] == 'Flossing' ? 'active' : '') ?>"><a href="<?php echo $pageUrl; ?>szolgaltatasok/flossing"><i class="fa fa-caret-right"></i> Flossing</a></li>
             <li><a href="#"><i class="fa fa-caret-right"></i> Branding (16)</a></li>
             <li><a href="#"><i class="fa fa-caret-right"></i> Typography (4)</a></li>
             <li><a href="#"><i class="fa fa-caret-right"></i> UI Design (5)</a></li>
