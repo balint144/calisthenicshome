70ed152ffbf9be04586282183ce9bf5c7bcf711c
Németh Bálint <nemeth.balint@telekom.hu>
2019-12-20 20:55:12 +0100

    árak kiegészítés

--- arak/index.php
+++ arak/index.php
@@ -32,6 +32,12 @@ include('../head.php');
                                     <li>Személyi edzés<span>9 500 Ft/alkalom</span></li>
                                     <li>Páros személyi edzés<span>5 000 Ft/fő/alkalom</span></li>
                                     <li>Kiscsoportos személyi edzés (3fő)<span>4 500 Ft/fő/alkalom</span></li>
+                                    <li>Funkcionális Mozgásminta Szűrés (FMS)<span>8 000 Ft/fő/alkalom</span></li>
+                                    <li>Konzultáció az egészséges életmód kialakításáról 60 perc<span>10 000 Ft/fő/alkalom</span></li>
+                                    <li>Meditáció 40 perc<span>9 500 Ft/fő/alkalom</span></li>
+                                    <li>Flosstréning 40 perc<span>9 500 Ft/fő/alkalom</span></li>
+                                    <li>Relaxáció 40 perc<span>9 500 Ft/fő/alkalom</span></li>
+                                    <li>Légzésterápia 40 perc<span>9 500 Ft/fő/alkalom</span></li>
                                 </ul>
                             </div>
                             <div class="item-cost">
